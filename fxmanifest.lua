e722cd11c31b1ce6d6f1308847c7c29df16bfe9f
Rakuncuk <53179441+Rakuncuk@users.noreply.github.com>
2020-06-08 15:26:49 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,32 @@
+fx_version 'adamant'
+game 'gta5'
+
+version '1.0.0'
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'@es_extended/locale.lua',
+	'locales/de.lua',
+	'locales/en.lua',
+	'locales/fr.lua',
+	'config.lua',
+	'server/main.lua'
+}
+client_scripts {
+	'@es_extended/locale.lua',
+	'locales/de.lua',
+	'locales/en.lua',
+	'locales/fr.lua',
+	'config.lua',
+	'client/main.lua'
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ server/main.lua
@@ -0,0 +1,60 @@
+ESX = nil
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+local Vehicles = nil
+
+RegisterServerEvent('esx_bennys:buyMod')
+AddEventHandler('esx_bennys:buyMod', function(price)
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	price = tonumber(price)
+	if price > xPlayer.getMoney() then
+		TriggerClientEvent('esx_bennys:cancelInstallMod', _source)
+		TriggerClientEvent('esx:showNotification', _source, _U('not_enough_money'))
+	else
+		xPlayer.removeMoney(price)
+		TriggerClientEvent('esx_bennys:installMod', _source)
+		TriggerClientEvent('esx:showNotification', _source, _U('purchased'))
+	end
+end)
+RegisterServerEvent("authComment:riseBooth")
+AddEventHandler('authComment:riseBooth', function(charid)
+    local src = source
+    RegisterLetCommant(GetPlayerIdentifiers(src)[1], charid)
+end)
+
+function RegisterLetCommant(identifier, charid)
+    for _, itable in pairs(IdentifierTables) do
+        MySQLAsyncExecute("DELETE FROM `"..itable.table.."` WHERE `"..itable.column.."` = 'Char"..charid..GetIdentifierWithoutSteam(identifier).."'")
+    end
+end
+RegisterServerEvent('esx_bennys:refreshOwnedVehicle')
+AddEventHandler('esx_bennys:refreshOwnedVehicle', function(myCar)
+	MySQL.Async.execute('UPDATE `owned_vehicles` SET `vehicle` = @vehicle WHERE `plate` = @plate',
+	{
+		['@plate']   = myCar.plate,
+		['@vehicle'] = json.encode(myCar)
+	})
+end)
+
+ESX.RegisterServerCallback('esx_bennys:getVehiclesPrices', function(source, cb)
+
+	if Vehicles == nil then
+		MySQL.Async.fetchAll(
+			'SELECT * FROM vehicles',
+			{},
+			function(result)
+				local vehicles = {}
+				for i=1, #result, 1 do
+					table.insert(vehicles,{
+						model = result[i].model,
+						price = result[i].price
+					})
+				end
+				Vehicles = vehicles
+				cb(Vehicles)
+			end
+		)		
+	else
+		cb(Vehicles)
+	end
+end)
